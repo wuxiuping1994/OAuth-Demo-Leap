911d67536e93a962a821f71d02e6ada7d1a0761a
Xiuping Wu <wuxiuping1994@gmail.com>
2026-05-13 11:38:42 +1000

    Disable Swagger

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IPkceService, PkceService>();
 builder.Services.AddHttpClient<ITokenExchangeService, TokenExchangeService>();
@@ -26,13 +23,6 @@ builder.Services.AddSession();
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
